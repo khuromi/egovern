fe8d0083c0dd1598b595343cb1e7f252de2fbfd6
Joanna Baylen <johnaizenbaylenoriente02@gmail.com>
2024-09-26 14:25:09 +0800

    removed unnecessary file

--- includes/topbar.php
+++ includes/topbar.php
@@ -1,6 +1,6 @@
 <nav class="topnav navbar navbar-expand shadow justify-content-between justify-content-sm-start navbar-light bg-white" id="sidenavAccordion">
             <button class="btn btn-icon btn-transparent-dark order-1 order-lg-0 me-2 ms-lg-2 me-lg-0" id="sidebarToggle"><i data-feather="menu"></i></button>
-            <a class="navbar-brand pe-3 ps-4 ps-lg-2" href="index.html">eGovern </a>
+            <a class="navbar-brand pe-3 ps-4 ps-lg-2" href="index.php">eGovern </a>
             <ul class="navbar-nav align-items-center ms-auto">
                 <li class="nav-item dropdown no-caret dropdown-user me-3 me-lg-4">
                     <a class="btn btn-icon btn-transparent-dark dropdown-toggle" id="navbarDropdownUserImage" href="javascript:void(0);" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img class="img-fluid" src="assets/img/illustrations/profiles/profile-1.png" /></a>
