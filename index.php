ee73083c8b4c5b4278e46f5727edfb38e517324b
markghenly <markghenlymurao@gmail.com>
2024-10-20 14:32:42 +0800

    nav date

--- index.php
+++ index.php
@@ -68,12 +68,7 @@ if (!$login->isLoggedIn()) {
                                     </div>
                                     
                                     <div class="col-12 col-xl-auto mt-4">
-                                        <div class="input-group input-group-joined border-0" style="width: 16.5rem">
-                                            <span class="input-group-text"><i class="text-primary" data-feather="calendar"></i></span>
-                                            <input class="form-control ps-0 pointer" id="litepickerRangePlugin" placeholder="Select date range..." />
-                                            
-                                                         
-                                        </div>
+                                       
                                         
                                         
                                     </div>
